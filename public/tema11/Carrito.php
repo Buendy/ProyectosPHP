9fe4e7644d59d7f14803b490f95cea408ab6326b
Daniel <danielbuendiasmr@gmail.com>
2018-10-19 16:50:17 +0000

    CARRITO 2.0

---
+++ public/tema11/Carrito.php
@@ -0,0 +1,100 @@
+<?php
+
+class Carrito
+{
+  private $productos = [];
+
+  public function meter($producto)
+  {
+    $this->productos[]=$producto;
+  }
+
+  public function mostrar()
+  {
+    $total = 0;
+    $totalIVA = 0;
+
+    echo '<div class="carrito">';
+
+    foreach ($this->productos as $producto) {
+      echo '<article class="lineacarrito">';
+      echo $producto->mostrar();
+      echo '</article>';
+      $total += $producto->precio();
+      $totalIVA += $producto->precioIVA();
+    }
+
+    echo '<div class="totalcarrito">Total: ' . $total . ' (' . $totalIVA . ' IVA Incluido)</div>';
+    echo '</div>';
+
+  }
+
+  public function quitar($indice)
+  {
+    unset($this->productos[$indice]);
+  }
+
+  public function masUnidad($indice)
+  {
+    $this->productos[$indice]->masUnidad();
+  }
+
+  public function menosUnidad($indice)
+  {
+    $this->productos[$indice]->menosUnidad();
+  }
+
+}
+
+
+class Producto
+{
+  private $nombre;
+  private $precio;
+  private $iva;
+  private $cantidad = 1;
+  
+
+  public function mostrar()
+  {
+    return "<p><span>({$this->cantidad}x)</span> {$this->nombre}: {$this->precio} &euro; + {$this->iva}% </p>";
+  }
+
+
+  public function precio()
+  {
+    return $this->precio * $this->cantidad;
+  }
+
+
+  public function precioIVA()
+  {
+    return round($this->precio * $this->cantidad * (1 + $this->iva/100),2);
+  }
+
+
+  public function __construct($nombre, $precio, $iva = 21)
+  {
+    $this->nombre=$nombre;
+    $this->precio=$precio;
+    $this->iva=$iva;
+  }
+
+
+  public function masUnidad($unidades = 1)
+  {
+    $this->cantidad += $unidades;
+  }
+
+
+  public function menosUnidad()
+  {
+    if($this->cantidad > 0){
+      $this->cantidad--;
+    }
+  }
+}
+
+
+
+ ?>
---
+++ public/tema11/ejemplo5.php
@@ -0,0 +1,33 @@
+<?php include('Carrito.php');?>
+
+
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Carrito de la compra</title>
+    <link rel="stylesheet" href="estilo.css">
+  </head>
+  <body>
+    <?php
+    $p1 = new Producto("Espuma de afeitar", 3.5);
+    $p2 = new Producto("Cereales de chocolate", 5.99);
+    $p3 = new Producto("Servilletas 20x20", 1.2);
+
+    $carrito = new Carrito();
+    $carrito->meter($p1);
+    $carrito->meter($p2);
+    $carrito->meter($p3);
+    $carrito->quitar(1);
+
+    $carrito->masUnidad(0);
+    $carrito->masUnidad(0);
+    $carrito->menosUnidad(2);
+    $carrito->menosUnidad(2);
+
+    $carrito->mostrar();
+
+     ?>
+
+  </body>
+</html>
