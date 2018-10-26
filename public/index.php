a082a4936e60ad616991d04955b77286d8d2d2d0
Daniel <danielbuendiasmr@gmail.com>
2018-10-26 19:13:33 +0000

    PDO A TOPE!

--- public/index.php
+++ public/index.php
@@ -118,6 +118,7 @@
 		<h3>Tema 12 PDO</h3>
 	<ul>
 		<li><a href="/tema12/ejemplo1.php">Ejemplo 1. Ejemplo de conexión a la base de datos</a></li>
+		<li><a href="/tema12/index.php">Ejemplo 1 completo</a></li>
 	</ul>
 	</li>
 
---
+++ public/tema12/index.php
@@ -0,0 +1,56 @@
+<?php
+
+//include('./lib/Dbpdo.php');
+include('./models/Usuario.php');
+
+echo 'Test de conexión: <br>';
+$db = new Dbpdo();
+//print_r($db->getConnection());
+
+echo '<pre>';
+//print_r($db->all());
+
+
+$usuario = new Usuario();
+//print_r($usuario->all());
+
+$datos = ['nombre'   => 'Me quieroMORIR', 'email' => 'muerte@php.es', 'password' => '123456', 'edad' => '22'];
+
+
+// $usuario_id = $usuario->insert($datos);
+//
+// echo 'El ID del nuevo usuario es: ' . $usuario_id;
+// print_r($usuario->all());
+
+
+
+//ESTO ES IUN INSERT
+// try {
+//   $usuario_id = $usuario->insert($datos);
+//   echo 'El ID del nuevo usuario es: ' . $usuario_id;
+//   print_r($usuario->all());
+// } catch(Exception $e){
+//   echo '<h1>ERROR: ' . $e->getMessage() . '</h1>';
+// }
+
+
+//ESTO ES UN UPDATE
+// $clave_dato =['id' => 4];
+//
+// try{
+//   $usuario->update($datos, $clave_dato);
+// }catch (Exception $e){
+//   echo '<h1>ERROR: ' . $e->getMessage() . '</h1>';
+// }
+
+// ESTO ES EL delete
+
+$clave_dato = ['id' => 7];
+
+try{
+  $usuario->delete($clave_dato);
+}catch (Exception $e){
+  echo '<h1>ERROR: ' . $e->getMessage() . '</h1>';
+}
+
+ ?>
